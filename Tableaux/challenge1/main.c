b43590f525cfe0ca3f4cfd140a1a4b18d94504bc
abdelghafourbatty <abdlghafourbatty15@gmail.com>
2024-09-26 17:55:38 +0200

    new

---
+++ Tableaux/challenge1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int table_1[]={1,44,35,4,5};
+    int i;
+    for(i=0;i<table_1[4];i++)
+    printf("%d\n",table_1[i]);
+
+    return 0;
+}
